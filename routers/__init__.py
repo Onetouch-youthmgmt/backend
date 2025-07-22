b3a9c543e84bdfadb25eb917e90ca83b3209a7df
Varun <varun.thaker@aiio.de>
2025-07-22 17:49:51 +0200

    removed custom auth and added prod docker

--- routers/__init__.py
+++ routers/__init__.py
@@ -1,7 +1,6 @@
 from .api_youth import router as api_youth_router
-from .api_auth import router as api_auth_router
 from .api_sabha_center import router as api_sabha_center_router     
 from .api_sabha import router as api_sabha_router
 from .api_attendance import router as api_attendance_router 
 from .api_health import router as api_health_router
-__all__ = ["api_youth_router", "api_auth_router", "api_sabha_center_router", "api_sabha_router", "api_attendance_router", "api_health_router"]
+__all__ = ["api_youth_router", "api_sabha_center_router", "api_sabha_router", "api_attendance_router", "api_health_router"]
